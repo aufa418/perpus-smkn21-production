8251f2974e5f546f425a9621ddaa5e0b5baa944b
aufa418 <aufacb124@gmail.com>
2024-04-20 23:43:52 +0700

    first commit

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -2,47 +2,6 @@ import React from 'react'
 import { Link } from '@inertiajs/react'
 
 function Navbar({ auth }) {
-    const isAuth = (user) => {
-        const CheckAdmin = (userCheck) => {
-            if (userCheck) {
-                return (
-                    <>
-                        <li><Link href={route('adminDashboard')} >Dashboard</Link></li>
-                        <li><Link href={route('pageimportbuku')} method='get' as='button' >Import Book</Link></li>
-                    </>
-                )
-            } else {
-                return (
-                    <li><Link href={route('userDashboard')} >Dashboard</Link></li>
-                )
-            }
-        }
-
-        if (user) {
-            return (
-                <>
-                    <div className="flex-none mx-5">
-                        <ul className="menu menu-horizontal px-1">
-                            <li>
-                                <details>
-                                    <summary>
-                                        Menu
-                                    </summary>
-                                    <ul className="p-1 bg-white rounded-t-none">
-                                        {CheckAdmin(user.isAdmin)}
-                                        <li><Link href={route('daftarbukupage')} method='get' as='button' >Daftar Buku</Link></li>
-                                        <li><Link href={route('logout')} method='post' as='button' >Sign Out</Link></li>
-                                    </ul>
-                                </details>
-                            </li>
-                        </ul>
-                    </div>
-                </>
-            )
-        } else {
-            return ""
-        }
-    }
     return (
         <>
             <div className="navbar bg-white text-gray-800">
