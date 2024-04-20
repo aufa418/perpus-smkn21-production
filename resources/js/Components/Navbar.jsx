428c42a231ce86d4e0f046d12691ee9c0b7cfc2d
aufa418 <aufacb124@gmail.com>
2024-04-21 00:26:44 +0700

    first commit

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({ auth }) {
         <>
             <div className="navbar bg-white text-gray-800">
                 <div className="flex-1">
-                    <Link className="btn btn-ghost text-xl text-gray-800" href="/">Perpustakaan SMKN 21 Jakarta</Link>
+                    <Link className="btn btn-ghost text-base text-gray-800" href="/">Perpustakaan SMKN 21 Jakarta</Link>
                 </div>
                 {auth ? <div className="flex-none mx-5">
                     <ul className="menu menu-horizontal px-1">
